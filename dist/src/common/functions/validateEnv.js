1eb39c0d75cfee8542dfc1be147275eedebc5f71
fadliarz <fadliar03@gmail.com>
2024-09-18 21:23:19 +0700

    update

--- dist/src/common/functions/validateEnv.js
+++ dist/src/common/functions/validateEnv.js
@@ -1,16 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const envalid_1 = require("envalid");
 function validateEnv() {
-    const env = (0, envalid_1.cleanEnv)(process.env, {
-        NODE_ENV: (0, envalid_1.str)({
-            choices: ["development", "production", "test"],
-        }),
-        ACCESS_TOKEN_PRIVATE_KEY: (0, envalid_1.str)(),
-        REFRESH_TOKEN_PRIVATE_KEY: (0, envalid_1.str)(),
-        PORT: (0, envalid_1.port)({ default: 5000 }),
-        NUM_PHYSICAL_CPUS: (0, envalid_1.num)(),
-    });
-    return env;
+    // const env = cleanEnv(process.env, {
+    //   NODE_ENV: str({
+    //     choices: ["development", "production", "test"],
+    //   }),
+    //   ACCESS_TOKEN_PRIVATE_KEY: str(),
+    //   REFRESH_TOKEN_PRIVATE_KEY: str(),
+    //   PORT: port({ default: 5000 }),
+    //   NUM_PHYSICAL_CPUS: num(),
+    // });
+    //
+    // return env;
+    return process.env;
 }
 exports.default = validateEnv;
--- src/common/functions/validateEnv.ts
+++ src/common/functions/validateEnv.ts
@@ -1,17 +1,17 @@
-import { cleanEnv, str, port, num } from "envalid";
-
 function validateEnv() {
-  const env = cleanEnv(process.env, {
-    NODE_ENV: str({
-      choices: ["development", "production", "test"],
-    }),
-    ACCESS_TOKEN_PRIVATE_KEY: str(),
-    REFRESH_TOKEN_PRIVATE_KEY: str(),
-    PORT: port({ default: 5000 }),
-    NUM_PHYSICAL_CPUS: num(),
-  });
+  // const env = cleanEnv(process.env, {
+  //   NODE_ENV: str({
+  //     choices: ["development", "production", "test"],
+  //   }),
+  //   ACCESS_TOKEN_PRIVATE_KEY: str(),
+  //   REFRESH_TOKEN_PRIVATE_KEY: str(),
+  //   PORT: port({ default: 5000 }),
+  //   NUM_PHYSICAL_CPUS: num(),
+  // });
+  //
+  // return env;
 
-  return env;
+  return process.env;
 }
 
 export default validateEnv;
